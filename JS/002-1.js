8cfd72b6f7e753e8576c7df17cb48d4355dbbda3
HoduGwaja <honeybutter9250@gmail.com>
2020-07-01 20:24:31 +0900

    Commit_2020.07.01

---
+++ JS/002-1.js
@@ -0,0 +1,3 @@
+function myFunction(){
+    document.getElementById("JS").innerHTML="짜잔";
+}
---
+++ JS/alert.js
@@ -0,0 +1,5 @@
+function alertFunction(){
+    window.alert("얘는 window.alert를 이용함");
+    alert("얘는 alert 함수 이용함");
+    alert("규카츠 먹고 싶다");
+}
