649f0a27cfd0eccdcddfbaa620c93f385cfc6408
HoduGwaja <honeybutter9250@gmail.com>
2020-07-15 20:22:41 +0900

    [commit 2020.07.15]

---
+++ React/egoing/BOM/WindowOpen.js
@@ -0,0 +1,19 @@
+function open1(){
+    window.open('https://www.dsm-dms.com/');
+}
+
+function open2(){
+    window.open('https://www.youtube.com/', '_self');
+}
+
+function open3(){
+    window.open('https://www.facebook.com/', '_blank');
+}
+
+function open4(){
+    window.open('https://www.notion.so/for-Premiere-Pro-e62d2437d69b4cf7aeea1a8f13b5a393', 'ot');
+}
+
+function open5(){
+    window.open('https://www.sandbox.co.kr/', '_blank', 'width=200, height=200, resizeable=no');
+}
