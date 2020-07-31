4f6391c09f5ba1cf2e27a775138fd5d3bdcb42c0
HoduGwaja <honeybutter9250@gmail.com>
2020-07-31 15:11:27 +0900

    [commit_2020.07.31]

---
+++ React/egoing/DOM/property.js
@@ -0,0 +1,18 @@
+function openChar(List, elmnt, color){
+    var i, tabContent, tablinks;
+    tabContent = document.getElementsByClassName("tabcontent");
+    for(i = 0; i<tabContent.length; i++){
+        tabContent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for(i  = 0; i<tablinks.length; i++){
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(List).style.display = "block";
+
+    elmnt.style.backgroundColor = color;
+
+   // document.getElementById("defaultOpen").click();
+}
