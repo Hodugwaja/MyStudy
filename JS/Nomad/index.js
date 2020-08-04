64c99c7ce4f66734dcd9f41a16016768ceab3535
HoduGwaja <honeybutter9250@gmail.com>
2020-08-04 12:10:52 +0900

    [commit | 2020.08.04 | array]

--- JS/Nomad/index.js
+++ JS/Nomad/index.js
@@ -1,14 +1,17 @@
-/*
-// String
-const What = "HoduGwaja"; // "" inside that quotes means String
-*/
+const Monday = "Mon";
+const Tuesday = "Tue";
+const Wendsday = "Wen";
+const Thursday = "Thr";
+const Friday = "Fri";
+const Saturday = "Sat";
+const Sunday = "Sun";
 
-/*
-// booolean
-const what = true; // "true and false"
-*/
+console.log(Monday, Tuesday, Wendsday, Thursday, Friday, Saturday, Sunday);
 
-/*Numbers
-    const what = 19721121; // just number
-*/
+// Make to arrays;
 
+
+const daysOfWeek = ["Mon", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun", Friday];
+console.log(daysOfWeek);
+
+console.log(daysOfWeek[2]); // computer starts with zero
